bfe580bbcad9222a2e04cfa2e8c22a81cf57aaeb
python060319 <51337287+python060319@users.noreply.github.com>
2019-09-16 17:59:14 +0300

    Create listbox.py

---
+++ listbox.py
@@ -0,0 +1,31 @@
+from tkinter import *
+
+top = Tk()
+top.title("File system project")
+top.geometry("200x250")
+
+lbl = Label(top, text="A list of favourite countries...")
+
+listbox = Listbox(top)
+
+listbox.insert(1, "India")
+
+listbox.insert(2, "USA")
+
+listbox.insert(3, "Japan")
+
+listbox.insert(4, "Austrelia")
+
+# this button will delete the selected item from the list
+
+def printit():
+    print(listbox.get(0, "end")[listbox.curselection()[0]])
+
+btn = Button(top, text="delete", command=printit)
+
+lbl.pack()
+
+listbox.pack()
+
+btn.pack()
+top.mainloop()
